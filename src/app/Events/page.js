eb734f9ff4585eb1dec636422454fd507d50977b
Your Name <gdikshan10@gmail.com>
2024-04-29 12:16:57 +0545

    eventdetail page

--- src/app/Events/page.js
+++ src/app/Events/page.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import {data} from '../../../dummy-data';
+import Eventlist from '../Components/Eventlist';
+import { getFeaturedEvents } from '../../../dummy-data';
 
 const page = () => {
+  const featuredEvents = getFeaturedEvents();
   return (
-    <div>
-      <h2 className='font-semibold text-zinc-500 my-5'>All Events Page</h2>
-      <div>
-        {data.map((item,idx)=>(
-          <li key={idx}>
-            <p>{item.title}</p>
-          </li>
-        ))}
+    // <div>
+    //   <h2 className='font-semibold text-zinc-500 my-5'>All Events Page</h2>
+    //   <div>
+    //     {data.map((item,idx)=>(
+    //       <li key={idx}>
+    //         <p>{item.title}</p>
+    //       </li>
+    //     ))}
+    //   </div>
+    // </div>
+    <div className="bg-grey-500 my-5">
+        <Eventlist featuredEvents={featuredEvents} />
       </div>
-    </div>
   )
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
-import Eventlist from "./Components/Eventlist";
+// import Eventlist from "./Components/Eventlist";
 import Layout from "./Components/Layout";
-import { getFeaturedEvents } from "../../dummy-data";
+// import { getFeaturedEvents } from "../../dummy-data";
 
 export default function Home() {
-  const  featuredEvents = getFeaturedEvents();
-  console.log(featuredEvents);
+  // const  featuredEvents = getFeaturedEvents();
   return (
     <Layout>
       <div className="bg-grey-500 my-5">
-        <Eventlist featuredEvents={featuredEvents} />
+        <h1 className="text-3xl font-bold text-center py-4">hi i am a index page </h1>
+        {/* <Eventlist featuredEvents={featuredEvents} /> */}
       </div>
     </Layout>
   );
